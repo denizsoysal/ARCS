cfeb67b2afdfe61fd9111ddd6b652d765dbdc101
Louis Hanut <louis.hanut@student.uclouvain.be>
2022-11-09 17:20:34 +0100

    Copied code for task_mediator from other project, needs to be updated

---
+++ include/task_mediator/task_mediator_petrinet.hpp
@@ -0,0 +1,34 @@
+/* ----------------------------------------------------------------------------
+ * Project Title,
+ * ROB @ KU Leuven, Leuven, Belgium
+ * Authors: 
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file task_mediator_petrinet.hpp
+ * @date November 09, 2022
+ **/
+
+#ifndef TASK_MEDIATOR_PETRINET_HPP
+#define TASK_MEDIATOR_PETRINET_HPP
+
+#include <coordination-libraries/petrinet/petrinet.h> //need to include it in CMake!
+#include <five_c/scheduler/eventloop_composition_and_execution/petrinet_scheduler.h>  
+
+// Tracking sources
+#define NUMBER_OF_TRACKING_SOURCES 2
+#define BOARD_IN_RANGE 0
+#define BOARD_DIRTY 0
+
+// Converting sinks
+#define NUMBER_OF_CONVERTING_SINKS 1
+#define IDENTIFY_DIRTY_PATCH_READY 0
+
+petrinet_t task_mediator_create_bringup_petrinet(const char *name); //the name bringup can be changed as it was created for the mobile platform specifically
+
+void task_mediator_reset_bringup_petrinet(petrinet_t *p);
+
+extern flag_token_conversion_map_t task_mediator_bringup_petrinet_flag_map;
+
+#endif // TASK_MEDIATOR_PETRINET_HPP
---
+++ src/task_mediator/task_mediator_petrinet.cpp
@@ -0,0 +1,77 @@
+/* ----------------------------------------------------------------------------
+ * Project Title,
+ * ROB @ KU Leuven, Leuven, Belgium
+ * Authors: 
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file task_mediator_petrinet.cpp
+ * @date November, 2022
+ **/
+
+#include "task_mediator/task_mediator_petrinet.hpp"
+
+// Tracking sources
+const char* bringup_tracking_source_names[] = {
+    "board_in_range",
+    "board_dirty"
+};
+bool* bringup_tracking_source_flags[NUMBER_OF_TRACKING_SOURCES];
+
+// Converting sinks
+const char* bringup_converting_sink_names[] = {
+    "identify_dirty_patch_ready"
+};
+bool* bringup_converting_sink_flags[NUMBER_OF_CONVERTING_SINKS];
+
+flag_token_conversion_map_t task_mediator_bringup_petrinet_flag_map = {
+        .converting_sources = {
+                .names = NULL,
+                .flags = NULL,
+                .number_of_flags = 0
+        },
+        .tracking_sources = {
+                .names = bringup_tracking_source_names,
+                .flags = bringup_tracking_source_flags,
+                .number_of_flags = NUMBER_OF_TRACKING_SOURCES
+        },
+        .converting_sinks = {
+                .names = bringup_converting_sink_names,
+                .flags = bringup_converting_sink_flags,
+                .number_of_flags = NUMBER_OF_CONVERTING_SINKS
+        },
+        .tracking_sinks = {
+                .names = NULL,
+                .flags = NULL,
+                .number_of_flags = 0
+        }
+};
+
+petrinet_t task_mediator_create_bringup_petrinet(const char *name) {
+    petrinet_t *p = init_petrinet(name);
+
+    place_t *p_board_in_range= create_place(p, bringup_tracking_source_names[BOARD_IN_RANGE]);
+    place_t *p_board_dirty = create_place(p, bringup_tracking_source_names[BOARD_DIRTY]);
+    place_t *p_identify_dirty_patch_ready = create_place(p, bringup_converting_sink_names[IDENTIFY_DIRTY_PATCH_READY]);
+
+    transition_behaviour_t b1 = {
+            .condition = cond_Black1,
+            .consumption_behaviour=consume_Black1,
+            .production_behaviour=produce_Black1
+    }; //this behavior is when all places before transition needs to be filled to fire the tr. After, they are all consumed and all output places are filled
+
+    // TRANSITIONS
+    transition_t *t1 = create_transition(p, "t1");
+    add_behaviour(t1, &b1);
+
+    agedge(p, p_kelo_ready, t1, "1", TRUE);
+    agedge(p, p_hokuyo_ready, t1, "1", TRUE);
+    agedge(p, t1, p_platform_operational, "1", TRUE);
+ 
+    return *p;
+}
+
+void task_mediator_reset_bringup_petrinet(petrinet_t *p) {
+        ;
+}
