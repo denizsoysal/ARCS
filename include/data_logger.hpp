0219e9a1fac087f0b7090d2347b3ef6eae039ad3
Brendan Pousett <brendan.pousett@student.kuleuven.be>
2022-12-08 06:48:47 +0100

    implemented data logger

---
+++ include/data_logger.hpp
@@ -0,0 +1,73 @@
+/* ----------------------------------------------------------------------------
+ * Project Title,
+ * ROB @ KU Leuven, Leuven, Belgium
+ * Authors: 
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file data_logger.hpp
+ * @date November 09, 2022
+ **/
+
+#ifndef data_logger_HPP
+#define data_logger_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+#include <five_c/activity/activity.h>
+#include "iiwa/iiwa_controller.hpp"
+#include <iiwa_activity.hpp>
+
+typedef struct data_logger_alg_s{
+    void (*create_lcsm)(activity_t*, const char* name_algorithm);
+    void (*resource_configure_lcsm)(activity_t*);
+    void (*destroy_lcsm)(activity_t*);
+}data_logger_t;
+
+// Parameters
+typedef struct data_logger_params_s{
+    FILE *fpt;
+    string fname;
+}data_logger_params_t;
+
+// Continuous state
+typedef struct data_logger_continuous_state_s{
+    iiwa_controller_continuous_state_t *controller_continuous_state;
+    iiwa_activity_continuous_state_t *iiwa_continuous_state;
+
+    struct timespec initial_time;
+    struct timespec log_time;
+    unsigned int time_ms;
+    
+    // state of abag
+    abag_state_t abag_state_log;
+    // outputs of controller
+    double cmd_jnt_vel_controller[LBRState::NUMBER_OF_JOINTS];
+    double cmd_jnt_torque_controller[LBRState::NUMBER_OF_JOINTS];
+    double cmd_wrench_controller[CART_VECTOR_DIM];
+    // commands received by iiwa
+    double cmd_jnt_vel_iiwa[LBRState::NUMBER_OF_JOINTS];
+    double cmd_jnt_torque_iiwa[LBRState::NUMBER_OF_JOINTS];
+    double cmd_wrench_iiwa[CART_VECTOR_DIM];
+    // state of the iiwa
+    double meas_jnt_pos_iiwa[LBRState::NUMBER_OF_JOINTS];
+	double meas_torques_iiwa[LBRState::NUMBER_OF_JOINTS];
+	double meas_ext_torques_iiwa[LBRState::NUMBER_OF_JOINTS];
+
+}data_logger_continuous_state_t;
+
+typedef struct data_logger_coordination_state_s {
+    // Activity LCSM
+    bool execution_request;
+    bool deinitialisation_request;
+    bool commanding_not_active;
+
+    // Mutex
+    pthread_mutex_t *sensor_lock, *actuation_lock, *goal_lock;
+} data_logger_coordination_state_t;
+
+extern const data_logger_t ec_data_logger;
+#endif // data_logger_HPP
---
+++ src/data_logger.cpp
@@ -0,0 +1,236 @@
+/* ----------------------------------------------------------------------------
+ * Project Title,
+ * ROB @ KU Leuven, Leuven, Belgium
+ * Authors: 
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file data_logger.cpp
+ * @date November 09, 2022
+ **/
+
+#include "data_logger.hpp"
+
+void data_logger_config(activity_t* activity){
+    // Remove config() from the eventloop schedule in the next iteration
+    remove_schedule_from_eventloop(&activity->schedule_table, "activity_config");
+    switch (activity->lcsm.state){
+        case CREATION:
+            add_schedule_to_eventloop(&activity->schedule_table, "creation");
+            break;
+        case RESOURCE_CONFIGURATION:
+            add_schedule_to_eventloop(&activity->schedule_table, "resource_configuration");
+            break;
+        case RUNNING:
+            add_schedule_to_eventloop(&activity->schedule_table, "running");
+            break;
+        case DONE:
+			break;
+    }
+};
+
+// Creation
+void data_logger_creation_coordinate(activity_t *activity){
+    // Coordinating own activity
+    if (activity->state.lcsm_flags.creation_complete){
+        activity->lcsm.state = RESOURCE_CONFIGURATION;
+        update_super_state_lcsm_flags(&activity->state.lcsm_flags, activity->lcsm.state);
+    }
+}
+
+void data_logger_creation_configure(activity_t *activity){
+    if (activity->lcsm.state != CREATION){
+        // Update schedule
+        add_schedule_to_eventloop(&activity->schedule_table, "activity_config");
+        remove_schedule_from_eventloop(&activity->schedule_table, "creation");
+    }
+}
+
+void data_logger_creation_compute(activity_t *activity){
+    activity->state.lcsm_flags.creation_complete = true;
+}
+
+void data_logger_creation(activity_t *activity){
+    data_logger_creation_compute(activity);
+    data_logger_creation_coordinate(activity);
+    data_logger_creation_configure(activity);
+}
+
+// Resource configuration
+void data_logger_resource_configuration_coordinate(activity_t *activity){
+     // Internal coordination
+    if (activity->state.lcsm_flags.resource_configuration_complete){
+        switch (activity->state.lcsm_protocol){ 
+            case EXECUTION:
+                activity->lcsm.state = RUNNING;
+                break;
+            case DEINITIALISATION:
+                activity->lcsm.state = DONE;
+                activity->state.lcsm_flags.deletion_complete = true;            
+                break;
+        }
+        update_super_state_lcsm_flags(&activity->state.lcsm_flags, activity->lcsm.state);
+    }
+}
+
+void data_logger_resource_configuration_configure(activity_t *activity){
+    if (activity->lcsm.state != RESOURCE_CONFIGURATION){
+        // Update schedule
+        add_schedule_to_eventloop(&activity->schedule_table, "activity_config");
+        remove_schedule_from_eventloop(&activity->schedule_table, "resource_configuration");
+        // Update flags for next visit to the resource configuration LCS 
+        activity->state.lcsm_flags.resource_configuration_complete = false;
+    }
+}
+
+void data_logger_resource_configuration_compute(activity_t *activity){
+    data_logger_params_t *params = (data_logger_params_t *) activity->conf.params;
+    data_logger_continuous_state_t *logger_state = (data_logger_continuous_state_t *) activity->state.computational_state.continuous;
+    if (activity->state.lcsm_protocol == EXECUTION){
+        params->fpt = fopen(params->fname, "a");
+	    fprintf(params->fpt, "%s, %s, %s, %s, %s, %s, %s\n", 
+            "time_ms",
+            "meas_jnt_pos_iiwa[6]",
+            "meas_jnt_torques_iiwa[6]",
+            "meas_ext_torques_iiwa[6]",
+            "cmd_jnt_vel_iiwa[6]",
+            "cmd_jnt_torque_iiwa[6]",
+            "cmd_wrench_iiwa[6]");
+        fclose(params->fpt);
+
+        timespec_get(&logger_state->initial_time, TIME_UTC);
+    }
+    activity->state.lcsm_flags.resource_configuration_complete  = true;
+}
+
+void data_logger_resource_configuration(activity_t *activity){
+    data_logger_resource_configuration_compute(activity);
+    data_logger_resource_configuration_coordinate(activity);
+    data_logger_resource_configuration_configure(activity);
+}
+
+// Running
+void data_logger_running_coordinate(activity_t *activity){
+    if (activity->state.lcsm_flags.running_complete){
+        activity->lcsm.state = RESOURCE_CONFIGURATION;
+        activity->state.lcsm_protocol = DEINITIALISATION;
+        update_super_state_lcsm_flags(&activity->state.lcsm_flags, activity->lcsm.state);
+    }
+}
+
+void data_logger_running_configure(activity_t *activity){
+    if (activity->lcsm.state != RUNNING){
+        // Update schedule
+        add_schedule_to_eventloop(&activity->schedule_table, "activity_config");
+        remove_schedule_from_eventloop(&activity->schedule_table, "running");
+        activity->state.lcsm_flags.running_complete = false;
+    }
+}
+
+void data_logger_running_compute(activity_t *activity){
+    data_logger_params_t *params = (data_logger_params_t *) activity->conf.params;
+    data_logger_coordination_state_t *coord_state = (data_logger_coordination_state_t *) activity->state.coordination_state;
+    data_logger_continuous_state_t *logger_state = (data_logger_continuous_state_t *) activity->state.computational_state.continuous;
+    iiwa_controller_continuous_state_t *controller_state = (iiwa_controller_continuous_state_t *) logger_state->controller_continuous_state;
+    iiwa_activity_continuous_state_t *iiwa_state = (iiwa_activity_continuous_state_t *) logger_state->iiwa_continuous_state;
+
+    timespec_get(&logger_state->log_time, TIME_UTC);
+    logger_state->time_ms = 
+        (logger_state->log_time.tv_sec - logger_state->initial_time.tv_sec) * 1000 + 
+        (logger_state->log_time.tv_nsec - logger_state->initial_time.tv_nsec) / 1000000;
+
+	pthread_mutex_lock(coord_state->sensor_lock);
+    memcpy(logger_state->meas_ext_torques_iiwa, iiwa_state->iiwa_state.iiwa_sensors.meas_ext_torques,
+        sizeof(iiwa_state->iiwa_state.iiwa_sensors));
+    memcpy(logger_state->meas_jnt_pos_iiwa, iiwa_state->iiwa_state.iiwa_sensors.meas_jnt_pos,
+        sizeof(iiwa_state->iiwa_state.iiwa_sensors.meas_jnt_pos));
+    memcpy(logger_state->meas_torques_iiwa, iiwa_state->iiwa_state.iiwa_sensors.meas_torques,
+        sizeof(iiwa_state->iiwa_state.iiwa_sensors.meas_torques));
+	pthread_mutex_unlock(coord_state->sensor_lock);
+    
+    pthread_mutex_lock(coord_state->actuation_lock);
+    // iiwa
+    memcpy(logger_state->cmd_jnt_vel_iiwa, iiwa_state->iiwa_state.iiwa_actuation_input.cmd_jnt_vel,
+        sizeof(iiwa_state->iiwa_state.iiwa_actuation_input.cmd_jnt_vel));
+    memcpy(logger_state->cmd_jnt_torque_iiwa, iiwa_state->iiwa_state.iiwa_actuation_input.cmd_torques,
+        sizeof(iiwa_state->iiwa_state.iiwa_actuation_input.cmd_torques));
+    memcpy(logger_state->cmd_wrench_iiwa, state->iiwa_state->iiwa_state.iiwa_actuation_input.cmd_jnt_vel,
+        sizeof(iiwa_state->iiwa_state.iiwa_actuation_input.cmd_wrench));
+
+    // controller abag state
+    pthread_mutex_unlock(coord_state->actuation_lock);
+
+    params->fpt = fopen(params->fname, "a");
+
+	fprintf(params->fpt, "%u, %f, %f, %f, %f, %f, %f\n", 
+        logger_state->time_ms;
+        logger_state->meas_jnt_pos_iiwa[6],
+        logger_state->meas_jnt_torques_iiwa[6],
+        logger_state->meas_ext_torques_iiwa[6],
+        logger_state->cmd_jnt_vel_iiwa[6],
+        logger_state->cmd_jnt_torque_iiwa[6],
+        logger_state->cmd_wrench_iiwa[6]);
+	fclose(params->fpt);
+}
+
+void data_logger_running(activity_t *activity){
+    data_logger_running_compute(activity);
+    data_logger_running_coordinate(activity);
+    data_logger_running_configure(activity);
+}
+
+// SCHEDULER 
+void data_logger_register_schedules(activity_t *activity){
+    schedule_t schedule_config = {.number_of_functions = 0};
+    register_function(&schedule_config, (function_ptr_t) data_logger_config, 
+        activity, "activity_config");
+    register_schedule(&activity->schedule_table, schedule_config, "activity_config");
+    
+    schedule_t schedule_creation = {.number_of_functions = 0};
+    register_function(&schedule_creation, (function_ptr_t) data_logger_creation, 
+        activity, "creation");
+    register_schedule(&activity->schedule_table, schedule_creation, 
+        "creation");
+
+    schedule_t schedule_resource_configuration = {.number_of_functions = 0};
+    register_function(&schedule_resource_configuration, (function_ptr_t) data_logger_resource_configuration, 
+        activity, "resource_configuration");
+    register_schedule(&activity->schedule_table, schedule_resource_configuration, 
+        "resource_configuration");
+
+    schedule_t schedule_running = {.number_of_functions = 0};
+    register_function(&schedule_running, (function_ptr_t) data_logger_running, 
+        activity, "running");
+    register_schedule(&activity->schedule_table, schedule_running, "running");
+}
+
+
+void data_logger_create_lcsm(activity_t* activity, const char* name_activity){
+    activity->conf.params = malloc(sizeof(data_logger_params_t));
+    activity->state.computational_state.continuous = malloc(sizeof(data_logger_continuous_state_t));
+    activity->state.coordination_state = malloc(sizeof(data_logger_coordination_state_t));
+}
+
+void data_logger_resource_configure_lcsm(activity_t *activity){
+    resource_configure_lcsm_activity(activity);
+    activity->mid = 2;
+    // Select the inital state of LCSM for this activity
+    activity->lcsm.state = CREATION;
+    activity->state.lcsm_protocol = EXECUTION;
+
+    // Schedule table (adding config() for the first eventloop iteration)
+    data_logger_register_schedules(activity);
+    add_schedule_to_eventloop(&activity->schedule_table, "activity_config");
+}
+
+void data_logger_destroy_lcsm(activity_t* activity){
+    destroy_activity(activity);
+}
+
+const data_logger_t ec_data_logger ={ 
+    .create_lcsm = data_logger_create_lcsm,
+    .resource_configure_lcsm = data_logger_resource_configure_lcsm,
+    .destroy_lcsm = data_logger_destroy_lcsm,
+};
+
