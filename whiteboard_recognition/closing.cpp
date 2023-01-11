95c05c1774237156f70f09de8a3ebca64a96d313
denizsoysal <deniz.soysal@kuleuven.be>
2023-01-11 10:32:56 +0100

    add file that merge all function and update closing.cpp

--- whiteboard_recognition/closing.cpp
+++ whiteboard_recognition/closing.cpp
@@ -83,63 +83,145 @@
 //try this shit out:
 //https://stackoverflow.com/questions/71446868/about-opencv-approxpolydp-function-program-cant-find-rectangle-that-i-want-if
 
+// #include "opencv2/imgcodecs.hpp"
+// #include "opencv2/highgui.hpp"
+// #include "opencv2/imgproc.hpp"
+// #include <iostream>
+// using namespace cv;
+// using namespace std;
+// Mat src_gray;
+// int thresh = 100;
+// RNG rng(12345);
+// void thresh_callback(int, void* );
+// int main( int argc, char** argv )
+// {
+//     Mat src = imread("../aaa.png",IMREAD_COLOR);
+//     cvtColor( src, src_gray, COLOR_BGR2GRAY );
+//     blur( src_gray, src_gray, Size(3,3) );
+//     const char* source_window = "Source";
+//     namedWindow( source_window );
+//     imshow( source_window, src );
+//     const int max_thresh = 255;
+//     createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
+//     thresh_callback( 0, 0 );
+//     waitKey();
+//     return 0;
+// }
+// void thresh_callback(int, void* )
+// {
+//     Mat canny_output;
+//     Canny( src_gray, canny_output, thresh, thresh*2 );
+//     vector<vector<Point> > contours;
+//     vector<vector<Point> > approx_contour;
+//     vector<Vec4i> hierarchy;
+//     findContours( canny_output, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE );
+//     Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+//     // // select last contour find 
+//     // int i = contours.size()-1;
+//     //draz all contour:
+//      for( size_t i = 0; i< contours.size(); i++ ){
+//         Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+//         approxPolyDP(contours[i],contours[i],100,1);
+//         drawContours( drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
+//         std::cout << contours[i].size();
+//         // putText(drawing,std::to_string(contours[i].size()),center,1,3,Scalar(255,0,0),2);
+
+//     }
+
+//     for(vector<Point>p:contours)
+//         for(Point k:p)
+//             circle(drawing,k,5,Scalar(255,255,255),FILLED);
+            
+
+//     namedWindow("final closed image", WINDOW_NORMAL);
+//     imshow("final closed image",drawing);
+//     cv::resizeWindow("final closed image", 300, 300);
+//     cv::waitKey(0);
+
+// }
+
+
+
+// CONTOUR DETECTIOM: 
+
+//https://stackoverflow.com/questions/13495207/opencv-c-sorting-contours-by-their-contourarea
+// comparison function object
+bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Point> contour2 ) {
+    double i = fabs( contourArea(cv::Mat(contour1)) );
+    double j = fabs( contourArea(cv::Mat(contour2)) );
+    return ( i < j );
+}
+
+
+
+//https://stackoverflow.com/questions/71446868/about-opencv-approxpolydp-function-program-cant-find-rectangle-that-i-want-if
+
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
-Mat src_gray;
+
 int thresh = 100;
-RNG rng(12345);
-void thresh_callback(int, void* );
-int main( int argc, char** argv )
-{
-    Mat src = imread("../aaa.png",IMREAD_COLOR);
-    cvtColor( src, src_gray, COLOR_BGR2GRAY );
-    blur( src_gray, src_gray, Size(3,3) );
-    const char* source_window = "Source";
-    namedWindow( source_window );
-    imshow( source_window, src );
-    const int max_thresh = 255;
-    createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
-    thresh_callback( 0, 0 );
-    waitKey();
-    return 0;
-}
-void thresh_callback(int, void* )
-{
-    Mat canny_output;
-    Canny( src_gray, canny_output, thresh, thresh*2 );
-    vector<vector<Point> > contours;
-    vector<vector<Point> > approx_contour;
-    vector<Vec4i> hierarchy;
-    findContours( canny_output, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE );
-    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-    // // select last contour find 
-    // int i = contours.size()-1;
-    //draz all contour:
-     for( size_t i = 0; i< contours.size(); i++ ){
-        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
-        approxPolyDP(contours[i],contours[i],100,1);
-        drawContours( drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
-        std::cout << contours[i].size();
-        // putText(drawing,std::to_string(contours[i].size()),center,1,3,Scalar(255,0,0),2);
 
+int main(){
+
+
+
+
+
+// I used cvtColor, inRange , bitwise_and, GaussianBlur, Canny functions before.
+vector<vector<Point>> contours; // to store contours
+vector<Vec4i> hierarchy;
+
+int mode = RETR_CCOMP;
+//int module = CHAIN_APPROX_NONE;
+//int module = CHAIN_APPROX_SIMPLE;
+//int module = CHAIN_APPROX_TC89_L1;
+int module = CHAIN_APPROX_TC89_KCOS;
+
+Mat canny_output,src_gray;
+Mat src = imread("../aaa.png",IMREAD_COLOR);
+cvtColor(src,src_gray,COLOR_BGR2GRAY    );
+blur( src_gray, src_gray, Size(3,3) );
+Canny( src_gray, canny_output, thresh, thresh*2 );
+
+
+findContours(canny_output, contours, hierarchy, mode, module);
+
+sort(contours.begin(), contours.end(), compareContourAreas);
+drawContours(image, contours, -1, Scalar(255, 0, 0), 1);
+imshow("ContourImage", image);
+
+vector<Point> contours_approx;
+
+for (int i = 0; i < 5; i++)
+{
+    double length = arcLength(contours[i], true);
+    approxPolyDP(contours[i], contours_approx, 0.1 * length, true);
+
+    if (contours_approx.size() == 4)
+    {
+        break;
     }
-
-    for(vector<Point>p:contours)
-        for(Point k:p)
-            circle(drawing,k,5,Scalar(255,255,255),FILLED);
-            
-
-    namedWindow("final closed image", WINDOW_NORMAL);
-    imshow("final closed image",drawing);
-    cv::resizeWindow("final closed image", 300, 300);
-    cv::waitKey(0);
-
+    contours_approx.clear();
 }
 
+for (Point i : contours_approx)
+{
+    cout << i << endl; // to check coordinate of vertex
+}
+
+drawContours(contour2, vector<vector<Point>>(1, contours_approx), -1, Scalar(0, 0, 255), 1);
+
+// to find vertex
+circle(contour2, Point(x0, y0), 5, Scalar(255, 0, 0), -1, -1, 0); // blue
+circle(contour2, Point(x1, y1), 5, Scalar(0, 255, 0), -1, -1, 0); // green
+circle(contour2, Point(x2, y2), 5, Scalar(0, 0, 255), -1, -1, 0); // red
+circle(contour2, Point(x3, y3), 5, Scalar(255, 255, 255), -1, -1, 0); // white
+imshow("contour2", contour2);
 
 
 
+}
---
+++ whiteboard_recognition/whiteboard_recognition.cpp
@@ -0,0 +1,135 @@
+/*
+
+Title: Algorithm that performs closing until we reach a plateau in our homogenity metric
+
+Explanation:
+- We want to perform closing on the image to be able to obtain a smooth surface for the 
+whiteboard (remove writing etc)
+- What we do is performing closing 
+- Then blurring the closed image, compute the sum of absolute difference between 
+the closed image and the blurred closed image --> this is our non-homogenity metric
+- Continue (increase the kernel of the closing operation)until the (non-homogenity metric at 
+time t+1)/(non-homogenity metric at time t)>threshold, with threshold being e.g. 0.7
+(this will mean that the non-homogenity does not increase that much)
+
+- Note that the non-homogenity metric decreases as the algorithm continue (because the 
+homogenity increases)
+
+- Note that as a first preprocessing step, we apply median blur to be able to remove the "salt and 
+pepper noise". We consider writing to be this kind of noise because they are high frequency
+and low size in space compared to other object in the image
+
+Assumptions:
+- We make the hypothesis that we are close to the whiteboard, 
+and that it occupies most of the image 
+- We make the hypothesis as the writing on the whiteboard are 
+smaller in size in the image frame compared to other object 
+on the image. So when doing closing they will disappear before the objects  
+
+
+
+P.S. : DOING IT WITH MEDIAN BLURRING INSTEAD OF CLOSING SEAMS BETTER
+P.S.2 : AS A HOMOGENEOUS METRIC, FFT CAN ALSO BE CONSIDERED
+
+*/
+
+
+
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include "opencv2/imgproc.hpp"
+#include <cmath>
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+cv::Mat adaptive_median_filtering(std::string path_to_img)
+{
+  //define the matrix we will need
+  cv::Mat src, image, sum, squared_diff, image_blurred, absolute_diff, closed_image, closed_image_blurred;
+  //read image
+  image = imread(path_to_img,IMREAD_COLOR);
+  //Blur the image with 3x3 Gaussian kernel
+  GaussianBlur(image, image_blurred, Size(11, 11), 0);
+  //compute pixel wise absolute diff between image and blurred one 
+  absdiff(image, image_blurred, absolute_diff); 	
+  //squared diff
+  squared_diff = absolute_diff.mul(absolute_diff);
+  //compute sum 
+  double sum_absolute_diff_initial = cv::sum(cv::sum(squared_diff))[0];
+  std::cout << "initial loss: " << sum_absolute_diff_initial << "\n";
+  //create an array of 2 elements: previous loss, current loss
+  double loss_array [2];
+  //previous loss at the beginning: initial loss
+  loss_array[0] = sum_absolute_diff_initial;
+  //loop for different kernel size (i) until we reach a plateau (minimum) 
+  //in term of loss (non-homogenity)
+  for(int i=0; i<=50; i++){
+    //closing operation 
+    // morphologyEx(image, closed_image, MORPH_CLOSE,getStructuringElement(MORPH_RECT,Size(2*i+1,2*i+1)));
+    medianBlur(image,closed_image,2*i+1);
+    //Blur the closed image with 3x3 Gaussian kernel
+    GaussianBlur(closed_image, closed_image_blurred, Size(11, 11), 0);
+    //compute pixel wise absolute diff between image and blurred one 
+    absdiff(closed_image, closed_image_blurred, absolute_diff); 	
+     //squared diff
+    squared_diff = absolute_diff.mul(absolute_diff);
+    //compute sum 
+    double sum_absolute_diff = cv::sum(cv::sum(squared_diff))[0];
+    std::cout << "loss of the closed image: " <<sum_absolute_diff << "\n";
+    //add this loss as current loss
+    loss_array[1] = sum_absolute_diff;
+    //compute ratio between current loss and previous loss
+    double ratio = loss_array[1] / loss_array[0];
+    std::cout << "loss improvement is: " << 1/ratio << "\n";
+    //if this ratio is higher than 0.8, this means that we don't improve a lot
+    //this threshold is user-defined
+      if(i>0 and ratio > 0.96){
+      std::cout << "Loss plateau reached !" << "\n";
+      std::cout<< "Final 'i' is:" << i << "\n" << "Final kernel size is then:" << 2*i+1 << "\n";
+      imwrite("test.jpg", closed_image); // A JPG FILE IS BEING SAVED
+      namedWindow("final closed image", WINDOW_NORMAL);
+      cv::resizeWindow("final closed image", 300, 300);
+      imshow("final closed image",closed_image);
+      waitKey();
+      break;
+    }
+    //if ratio threshold not reached, add this loss as previous loss
+    loss_array[0] = sum_absolute_diff;
+  }
+  return closed_image;
+}
+
+cv::Mat segment_otsu(cv::Mat image_to_segment)
+{
+  const char* window_name = "Region Growing";
+
+  Mat img_gray,img_thresh,img_thresh_dilated,img_thresh_dilated_eroded;
+  cvtColor( image_to_segment, img_gray, COLOR_BGR2GRAY);
+  cv::threshold(img_gray, img_thresh, 0, 255, THRESH_BINARY | THRESH_OTSU);
+  namedWindow( window_name, WINDOW_NORMAL );
+  cv::resizeWindow(window_name, 300, 300);
+  erode(img_thresh, img_thresh_dilated,getStructuringElement(MORPH_RECT,Size(55,55)));
+  dilate(img_thresh_dilated, img_thresh_dilated_eroded,getStructuringElement(MORPH_RECT,Size(55,55)));
+
+  imshow(window_name,img_thresh_dilated_eroded);
+
+
+  waitKey();
+
+  return img_thresh_dilated_eroded;
+}
+
+
+
+
+int main( int argc, char** argv )
+{ 
+    cv::Mat closed_image = adaptive_median_filtering("../323928745_1183154885902175_2448762449208961810_n.jpg");
+    cv::Mat image_after_otsu =  segment_otsu(closed_image);
+}
+
